81ed04191c97f9ab2aca7e0b82992687e1ebe624
Junhao Liao <junhao@junhao.ca>
2019-12-30 02:10:50 -0500

    Change port scanning method to check ports not occupied by current user

--- checkPort.sh
+++ checkPort.sh
@@ -1,8 +1,7 @@
 rm -f portscan.log
-
 for i in {1..99}
 do
 echo "checking port $i"
 port=$(($i+5900))
-lsof -i:$port | grep 'vnc' > /dev/null && echo "$i" >> portscan.log
+sh -c "nc -z -nv 127.0.0.1 $port 2>&1" | grep 'succeeded' > /dev/null && echo "$i" >> portscan.log
 done
