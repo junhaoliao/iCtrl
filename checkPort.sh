806c95ceb0266559579c6af090012f91100fb3fd
Junhao Liao <junhao@junhao.ca>
2020-07-08 01:51:38 -0400

    Update port checking script to better support
    Update port checking script to better support different prompts of nc

--- checkPort.sh
+++ checkPort.sh
@@ -1,7 +1,8 @@
+#!/bin/sh
 rm -f portscan.log
 for i in {1..99}
 do
 echo "checking port $i"
 port=$(($i+5900))
-sh -c "nc -z -nv 127.0.0.1 $port 2>&1" | grep 'open' > /dev/null && echo "$i" >> portscan.log
+sh -c "nc -z -nv 127.0.0.1 $port 2>&1" | grep 'open\|succeeded' > /dev/null && echo "$i">> ./portscan.log
 done
