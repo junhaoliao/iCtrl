8a82b612d168f0120c4d8f06b7ec1fe65ee74ed6
Kruz.x <xuyizhong201709@163.com>
2021-04-11 13:21:22 +0800

    runable login demo

--- remotron.js/main.js
+++ remotron.js/main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1440,
+    height: 768,
     webPreferences: {
       // preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
@@ -19,7 +19,7 @@ function createWindow () {
   // mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
