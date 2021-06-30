340f3610236245ab64dc0bc0af1c1880a7765070
Junhao Liao <junhao@junhao.ca>
2021-06-30 16:00:05 +0800

    implement SFTP file manager

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,48 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-import Home from "./components/Home";
+import Home from './components/Home';
+import FileManager from './components/FileManager';
+import React from 'react';
 
-function App() {
-  return (
-      // eslint-disable-next-line react/jsx-no-undef
-        <Router>
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-    </Router>
-  );
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            profiles: {}
+        };
+    }
+
+    componentDidMount() {
+        if (Object.keys(this.state.profiles).length === 0) {
+            fetch('/profiles')
+                .then(res => res.json())
+                .then(result => {
+                    console.log('fetch');
+                    this.setState({
+                        profiles: result
+                    });
+                });
+        }
+    }
+
+    render() {
+        if (Object.keys(this.state.profiles).length === 0) {
+            return null;
+        }
+
+        return (
+            <Router>
+                <Switch>
+                    <Route path="/fm/:session_id" render={(props) => (
+                        <FileManager {...props} profiles={this.state.profiles}/>
+                    )}/>
+                    <Route path="/">
+                        <Home/>
+                    </Route>
+                </Switch>
+            </Router>
+        );
+    }
+
 }
-
-export default App;
---
+++ client/src/components/FileManager/CustomToolbar.js
@@ -0,0 +1,158 @@
+import {GridToolbarContainer} from '@material-ui/data-grid';
+import {Button, IconButton, InputAdornment, Menu, MenuItem, OutlinedInput} from '@material-ui/core';
+import React from 'react';
+import {GetApp, VisibilityOff} from '@material-ui/icons';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import {DensityComfortableIcon, DensityCompactIcon, DensityStandardIcon} from '../../icons';
+
+export default class CustomToolbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.fm = props.fm;
+
+        this.state = {
+            densityMenuAnchorEl: null
+        };
+    }
+
+
+    handleClose = (ev) => {
+        this.setState({
+            densityMenuAnchorEl: null
+        });
+    };
+
+    handleMenuOpen = (ev) => {
+        this.setState({
+            densityMenuAnchorEl: ev.currentTarget
+        });
+    };
+
+    handleDensityChange = (ev) => {
+        this.fm.setState({
+            density: ev.currentTarget.getAttribute('aria-label')
+        });
+        this.setState({
+            densityMenuAnchorEl: null
+        });
+    };
+
+    handleHiddenFiles = (ev) => {
+        this.fm.showHidden = !this.fm.showHidden;
+        this.fm.setState({
+            filesDisplaying: this.fm.showHidden ? this.fm.files : this.fm.nonHiddenFiles(),
+        });
+    };
+
+    handleDownload = (ev) => {
+        const url = `http://localhost:5000/sftp_dl/${this.fm.session_id}?` +
+            `cwd=${this.fm.state.cwd}&` +
+            `files=${JSON.stringify(this.fm.selected)}`;
+        const a = document.createElement('a');
+        a.download = '';
+        a.href = url;
+        a.click();
+    };
+
+    handleCwdSubmit = (ev) => {
+        const path = this.fm.state.cwdInput;
+        this.fm.loadDir(path);
+
+        ev.preventDefault();
+    };
+
+    handleCwdInputChange = (ev) => {
+        this.fm.setState({
+            cwdInput: ev.target.value
+        });
+    };
+    handleCwdInputBlur = (ev) => {
+        if (ev.relatedTarget && ev.relatedTarget.id === 'enter_button') {
+            return;
+        }
+        this.fm.setState({
+            cwdInput: this.fm.state.cwd,
+            cwdInputErr: null
+        });
+    };
+
+    handleUpOneLevel = (ev) => {
+        const cwd = this.fm.state.cwd;
+        const path = cwd.substring(0, cwd.lastIndexOf('/'));
+        if (path === '') {
+            this.fm.loadDir('/');
+        } else {
+            this.fm.loadDir(path);
+        }
+
+    };
+
+
+    render() {
+        return (<GridToolbarContainer>
+            <IconButton aria-label={'up'} onClick={this.handleUpOneLevel} color={'primary'}>
+                <ArrowUpwardIcon/>
+            </IconButton>
+            <form onSubmit={this.handleCwdSubmit} autoComplete="off">
+                <OutlinedInput style={{width: '30vw'}}
+                               onBlur={this.handleCwdInputBlur} onChange={this.handleCwdInputChange}
+                               value={this.fm.state.cwdInput}
+                               error={Boolean(this.fm.state.cwdInputErr)}
+                               endAdornment={
+                                   <InputAdornment position="end">
+                                       <IconButton color={'primary'} id={'enter_button'} edge={'end'}
+                                                   aria-label={'enter'}
+                                                   onClick={this.handleCwdSubmit}>
+                                           <KeyboardArrowRightIcon/>
+                                       </IconButton>
+                                   </InputAdornment>
+                               }
+                               inputProps={{'aria-label': 'current-directory'}}/>
+            </form>
+            <Button color={'primary'} onClick={this.handleDownload}
+                    startIcon={<GetApp/>}>
+                Download
+            </Button>
+            <Button color={'primary'} onClick={() => {
+                this.handleHiddenFiles();
+            }}
+                    startIcon={<VisibilityOff/>}>
+                {this.fm.showHidden ? 'Hide' : 'Show'} Hidden Files
+            </Button>
+            <Button color={'primary'} aria-controls="density" aria-haspopup="true" onClick={this.handleMenuOpen}
+                    startIcon={<svg style={{position: 'relative', top: -2}} className="MuiSvgIcon-root"
+                                    focusable="false"
+                                    aria-hidden="true">
+                        <path d="M21,8H3V4h18V8z M21,10H3v4h18V10z M21,16H3v4h18V16z"/>
+                    </svg>}>
+                Density
+            </Button>
+            <Menu
+                id="density"
+                anchorEl={this.state.densityMenuAnchorEl}
+                keepMounted
+                open={Boolean(this.state.densityMenuAnchorEl)}
+                onClose={this.handleClose}
+            >
+                <MenuItem aria-label="compact" onClick={this.handleDensityChange}>
+                    <DensityCompactIcon/>
+                    Compact
+                </MenuItem>
+                <MenuItem aria-label="standard" onClick={this.handleDensityChange}>
+                    <DensityStandardIcon/>
+                    Standard
+                </MenuItem>
+                <MenuItem aria-label="comfortable" onClick={this.handleDensityChange}>
+                    <DensityComfortableIcon/>
+                    Comfortable
+                </MenuItem>
+            </Menu>
+
+        </GridToolbarContainer>);
+    }
+}
+
+
+
+
