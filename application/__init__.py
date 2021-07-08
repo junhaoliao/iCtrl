fdebc92a9180388a5521b56cc768b82eb3205aed
Junhao Liao <junhao@junhao.ca>
2021-07-08 19:20:38 +0800

    enable persistent HTTP connections (keep-alive)

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from werkzeug.serving import WSGIRequestHandler
 
 from application.Profile import Profile
+
+# enable persistent HTTP connections (keep-alive)
+WSGIRequestHandler.protocol_version = "HTTP/1.1"
 
 app = Flask(__name__, static_folder="../client/build/static", template_folder="../client/build")
 profiles = Profile()
