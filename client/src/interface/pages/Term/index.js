d2d73d3daddd291c658a9306832524cd92daf080
Junhao Liao <junhao@junhao.ca>
2021-07-11 15:20:43 +0800

    support Terminal resizing on window resize

--- client/src/interface/pages/Term/index.js
+++ client/src/interface/pages/Term/index.js
@@ -1,44 +1,60 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React from 'react';
-import { Terminal } from 'xterm';
+import {Terminal} from 'xterm';
 import 'xterm/css/xterm.css';
-import { AttachAddon } from 'xterm-addon-attach';
-import { FitAddon } from 'xterm-addon-fit';
+import {AttachAddon} from 'xterm-addon-attach';
+import {FitAddon} from 'xterm-addon-fit';
+import axios from 'axios';
 
 export default class Term extends React.Component {
     constructor(props) {
         super(props);
         const {match: {params}} = this.props;
         this.session_id = params.session_id;
+        this.term_id = null;
     }
-    
+
     componentDidMount() {
-        const request = new XMLHttpRequest();
-        request.addEventListener('load', (ev) => {
-            const term_id = request.response;
+        axios.post(`/terminal`, {
+            session_id: this.session_id
+        }).then(response => {
             const term = new Terminal();
             term.open(document.getElementById('terminal'));
-            const socket = new WebSocket(`ws://localhost:8000/${term_id}`);
+
+            this.term_id = response.data;
+            const socket = new WebSocket(`ws://localhost:8000/${this.term_id}`);
             const attachAddon = new AttachAddon(socket);
+            term.loadAddon(attachAddon);
+
+            term.onResize(({cols, rows}) => {
+                if (this.term_id != null) {
+                    axios.patch(`/terminal_resize`, {
+                        session_id: this.session_id,
+                        term_id: this.term_id,
+                        w: cols,
+                        h: rows
+                    }).then(response => {
+                        console.log(response);
+                    }).catch(error => {
+                        console.log(error);
+                    });
+                }
+            });
             const fitAddon = new FitAddon();
             term.loadAddon(fitAddon);
-            term.loadAddon(attachAddon);
             fitAddon.fit();
             window.onresize = () => {
-                // console.log('resizing')
                 fitAddon.fit();
-            }
+            };
+
+        }).catch(error => {
+            console.log(error);
         });
-        const formData = new FormData();
-        formData.append('session_id', this.session_id);
-        request.open('POST', '/terminal');
-        request.send(formData);
     }
 
     render() {
-        ***REMOVED***
         return (
-            <div id="terminal" style={{ height: '100vh' }}></div>
-        )
+            <div id="terminal" style={{height: '100vh'}}/>
+        );
     }
 }
