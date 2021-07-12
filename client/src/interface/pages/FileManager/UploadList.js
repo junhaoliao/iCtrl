23491f4c22a61f69ec88f0edbf94f03627c43bf7
Junhao Liao <junhao@junhao.ca>
2021-07-13 01:49:31 +0800

    fix upload item name overflowing

--- client/src/interface/pages/FileManager/UploadList.js
+++ client/src/interface/pages/FileManager/UploadList.js
@@ -1,19 +1,22 @@
-import {Box, Divider} from '@material-ui/core';
+import {Box, Divider, List} from '@material-ui/core';
 
 import UploadItem from './UploadItem';
 
 const UploadList = (props) => {
     const {fm} = props;
     const uploadItems = fm.state.uploadProgress;
-    const children = uploadItems.map((_, idx) => (
-        <div key={idx}>
-            <UploadItem fm={fm} uploadProgressIdx={idx}/>
-            {(idx !== uploadItems.length - 1) && <Divider/>}
-        </div>
-    ));
-    return (<Box margin={'16px 20px 16px 20px'} display={'vertical'}>
+    const children = []
+    uploadItems.forEach((_, idx) => {
+        children.push(
+            <UploadItem key={idx} fm={fm} uploadProgressIdx={idx}/>
+        )
+        if (idx !== uploadItems.length - 1){
+            children.push(<Divider/>)
+        }
+    });
+    return (<List margin={'16px 20px 16px 20px'}>
         {children}
-    </Box>);
+    </List>);
 };
 
 export default UploadList;
