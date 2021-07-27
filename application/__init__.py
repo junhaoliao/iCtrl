4c6c7e0ada5505dc39cb4aae9af4f65faed352f5
Junhao Liao <junhao@junhao.ca>
2021-07-27 21:37:42 +0800

    simplify the logic to serve the react build

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, send_from_directory
+import os
+import sys
+
+from flask import Flask, send_from_directory
 from werkzeug.serving import WSGIRequestHandler
 
 from application.Profile import Profile
@@ -6,23 +9,22 @@ from application.Profile import Profile
 # enable persistent HTTP connections (keep-alive)
 WSGIRequestHandler.protocol_version = "HTTP/1.1"
 
-app = Flask(__name__, static_folder="../client/build/static", template_folder="../client/build")
+if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    app = Flask(__name__, static_folder=os.path.join(os.getcwd(), 'client'))
+else:
+    app = Flask(__name__, static_folder="../client/build")
+
 profiles = Profile()
 
 from application.routes import *
 
 if not app.debug:
-    @app.errorhandler(404)
-    def not_found(_):
-        if request.path == '/icon.png':
-            return send_from_directory(app.template_folder, 'icon.png')
-        elif request.path == '/manifest.json':
-            return send_from_directory(app.template_folder, 'manifest.json')
+    # Reference: https://stackoverflow.com/questions/44209978/serving-a-front-end-created-with-create-react-app-with
+    # -flask
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def serve(path):
+        if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+            return send_from_directory(app.static_folder, path)
         else:
-            print(request.path)
-        return render_template('index.html')
-
-
-    @app.route("/")
-    def react():
-        return render_template('index.html')
+            return send_from_directory(app.static_folder, 'index.html')
