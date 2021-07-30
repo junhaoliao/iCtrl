635b0b8c422850ec8e4a82a9076f3cedcb6e5809
Junhao Liao <junhao@junhao.ca>
2021-07-30 22:33:36 +0800

    add publishing script

--- desktop_client/main.js
+++ desktop_client/main.js
@@ -1,24 +1,24 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow, nativeImage} = require('electron')
-if (require('electron-squirrel-startup')) return app.quit();
+require('update-electron-app')()
+const {handleSquirrelEvent} = require('./squirrel_event')
 
-const {spawn} = require("child_process")
-const {get} = require('http')
-const {createServer} = require("net")
-const {resolve} = require('path')
-
-function getFreePort() {
-    const srv = createServer()
-    srv.listen()
-    const port = srv.address()['port']
-    srv.close()
-    return port
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
 }
 
-const mainPort = getFreePort()
+// Modules to control application life and create native browser window
+const {app, BrowserWindow, nativeImage} = require('electron');
+const {spawn} = require('child_process');
+const {get} = require('http');
+const {resolve} = require('path');
+
+const {getFreePort} = require('./utils');
+
+const mainPort = getFreePort();
 
 // launch the backend
-spawn('ictrl_be.exe', [mainPort], {cwd: resolve(__dirname, 'ictrl_be')})
+spawn('ictrl_be.exe', [mainPort], {cwd: resolve(__dirname, 'ictrl_be')});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -30,24 +30,24 @@ app.whenReady().then(() => {
         height: 600,
         show: false,
         autoHideMenuBar: true,
-            webPreferences: {
-                nativeWindowOpen: true
-            }
-    })
+        webPreferences: {
+            nativeWindowOpen: true
+        }
+    });
 
-    mainWindow.loadURL(`http://localhost:${mainPort}`)
+    mainWindow.loadURL(`http://localhost:${mainPort}`);
 
-    mainWindow.removeMenu()
+    mainWindow.removeMenu();
     mainWindow.setAppDetails({
         appId: 'iCtrl'
-    })
+    });
 
     mainWindow.once('ready-to-show', () => {
-        mainWindow.maximize()
-    })
+        mainWindow.maximize();
+    });
 
     mainWindow.webContents.on('new-window', (event, url) => {
-        event.preventDefault()
+        event.preventDefault();
 
         const newWindow = new BrowserWindow({
             width: 800,
@@ -57,44 +57,44 @@ app.whenReady().then(() => {
             webPreferences: {
                 nativeWindowOpen: true
             }
-        })
-        newWindow.loadURL(url)
+        });
+        newWindow.loadURL(url);
 
-        newWindow.removeMenu()
+        newWindow.removeMenu();
         newWindow.once('ready-to-show', () => {
-            const url = newWindow.getURL()
+            const url = newWindow.getURL();
             newWindow.setAppDetails({
                 appId: url
-            })
-            const split = url.split('/')
-            const sessionId = split[split.length - 1]
-            const feature = split[split.length - 2]
+            });
+            const split = url.split('/');
+            const sessionId = split[split.length - 1];
+            const feature = split[split.length - 2];
             get({
                 hostname: 'localhost',
                 port: mainPort,
                 path: `/favicon/${feature}/${sessionId}`,
             }, (res) => {
                 res.on('data', (data) => {
-                    const icon = nativeImage.createFromBuffer(data)
-                    newWindow.setIcon(icon)
+                    const icon = nativeImage.createFromBuffer(data);
+                    newWindow.setIcon(icon);
                 });
             });
 
-            newWindow.maximize()
-        })
+            newWindow.maximize();
+        });
 
-    })
+    });
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
-})
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-    app.quit()
-})
+app.on('window-all-closed', () => {
+    app.quit();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
