1bcac1309fd68fb17492ae68e8a910765cdacd30
Junhao Liao <junhao@junhao.ca>
2021-07-31 20:32:07 +0800

    add a prompt in the Windows make script to ensure the maker update the version info and make a commit before making

--- win64_build.bat
+++ win64_build.bat
@@ -1,3 +1,20 @@
+@echo off
+
+echo ------------------------   Check Uncommitted Changes    ------------------------
+echo If you see any lines below, there are uncommitted changes:
+git status -s
+
+echo ------------------------  Confirm Desktop Client Info   ------------------------
+echo Have you updated the version info in desktop_client/package.json and made a
+echo  commit? (y to proceed)
+set /p confirm=""
+
+if not "%confirm%"=="y" (
+    echo Please update the version info first
+    goto :EOF
+)
+
+@echo on
 :: build the web client
 cd client
 call npm run build
@@ -11,4 +28,4 @@ pyinstaller --noconfirm --clean win64_ictrl_be.spec
 
 :: make the desktop client
 cd desktop_client
-call npm run make
+call npm run publish
