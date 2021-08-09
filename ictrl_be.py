7cea0cac5f08836be52bba2069f0a2cf6ee71dca
Junhao Liao <junhao@junhao.ca>
2021-08-09 11:59:11 +0800

    [Backend] do NOT listen on all interfaces when launching as a local server

--- ictrl_be.py
+++ ictrl_be.py
@@ -5,6 +5,9 @@ from application import app
 if __name__ == '__main__':
     try:
         port = sys.argv[1]
+        host = '127.0.0.1'
     except IndexError:
+        # TODO: change this to 80 in production mode
         port = 5000
-    app.run(host='0.0.0.0', port=port)
+        host = '0.0.0.0'
+    app.run(host=host, port=port)
