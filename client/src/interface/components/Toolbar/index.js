08a18513bef1ff0ebe6174aab64b8b523b6ea1c6
Junhao Liao <junhao@junhao.ca>
2021-08-22 13:20:06 -0400

    [Toolbar, VNCViewer] support shift key

--- client/src/interface/components/Toolbar/index.js
+++ client/src/interface/components/Toolbar/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Chip, IconButton} from '@material-ui/core';
 import {KeyboardArrowDown} from '@material-ui/icons';
 
-import './index.css'
+import './index.css';
 
 export default class Toolbar extends React.Component {
     constructor(props) {
@@ -13,68 +13,70 @@ export default class Toolbar extends React.Component {
     }
 
     handleChipClick = (key) => {
-        const {onToolbarSendKey:sendKey} = this.props;
-        const {pressed} = this.state
+        const {onToolbarSendKey: sendKey} = this.props;
+        const {pressed} = this.state;
         switch (key) {
             case 'Ctrl':
-            case '⌘':
             case 'Alt':
-                if (!pressed.has(key)){
+            case 'Shift':
+            case '⌘':
+                if (!pressed.has(key)) {
                     // previously not toggled
                     sendKey(key, true);
-                    pressed.add(key)
+                    pressed.add(key);
 
-                    const textarea = document.getElementsByTagName('textarea')[0]
-                    const cancelAll = (_)=>{
-                        setTimeout(()=>{
-                            for (let pressedKey of pressed){
-                            sendKey(pressedKey, false);
-                        }
-                        this.setState({pressed: new Set()})
-                        textarea.removeEventListener('keydown', cancelAll, true)
-                        },0)
+                    const textarea = document.getElementsByTagName('textarea')[0];
+                    const cancelAll = (_) => {
+                        setTimeout(() => {
+                            for (let pressedKey of pressed) {
+                                sendKey(pressedKey, false);
+                            }
+                            this.setState({pressed: new Set()});
+                            textarea.removeEventListener('keydown', cancelAll, true);
+                        }, 0);
 
-                    }
-                    textarea.removeEventListener('keydown', cancelAll, true)
-                    textarea.addEventListener('keydown', cancelAll, true)
+                    };
+                    textarea.removeEventListener('keydown', cancelAll, true);
+                    textarea.addEventListener('keydown', cancelAll, true);
                 } else {
                     // previously toggled
                     sendKey(key, false);
-                    pressed.delete(key)
+                    pressed.delete(key);
                 }
                 break;
             case 'Tab':
             case 'Esc':
             case 'Delete':
-                sendKey(key)
+                sendKey(key);
                 break;
             case 'Ctrl+Alt+Delete':
-                this.props.onCtrlAltDelete()
+                this.props.onCtrlAltDelete();
                 break;
             default:
                 break;
         }
-        this.setState({pressed: pressed})
+        this.setState({pressed: pressed});
     };
 
     render() {
-        const {onToolbarHide:handleToolbarHide} = this.props;
-        const {pressed} = this.state
+        const {onToolbarHide: handleToolbarHide} = this.props;
+        const {pressed} = this.state;
         const keyList = [
-                        {label: 'Ctrl'},
-                        {label: '⌘'},
-                        {label: 'Alt'},
-                        {label: 'Tab'},
-                        {label: 'Esc'},
-                        {label: 'Delete'},
-                    ];
-        if (this.props.onCtrlAltDelete){
-            keyList.push({label: 'Ctrl+Alt+Delete'})
+            {label: 'Ctrl'},
+            {label: 'Alt'},
+            {label: 'Shift'},
+            {label: '⌘'},
+            {label: 'Tab'},
+            {label: 'Esc'},
+            {label: 'Delete'},
+        ];
+        if (this.props.onCtrlAltDelete) {
+            keyList.push({label: 'Ctrl+Alt+Delete'});
         }
         return (
             <div id={'toolbar'}>
                 <IconButton
-                    style={{marginLeft:6}}
+                    style={{marginLeft: 6}}
                     color={'info'}
                     onClick={handleToolbarHide}
                 >
@@ -86,9 +88,12 @@ export default class Toolbar extends React.Component {
                               label={item.label}
                               color="primary"
                               style={{
-                              marginLeft:12}}
-                              variant={pressed.has(item.label)?'filled':'outlined'}
-                              onClick={()=>{this.handleChipClick(item.label)}}
+                                  marginLeft: 12
+                              }}
+                              variant={pressed.has(item.label) ? 'filled' : 'outlined'}
+                              onClick={() => {
+                                  this.handleChipClick(item.label);
+                              }}
                         />))}
             </div>
         );
