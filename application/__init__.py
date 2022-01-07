780367977240fa96484bb1a3b2baec1f7406555e
Junhao Liao <junhao@junhao.ca>
2022-01-07 00:05:31 -0500

    [Profile] change source files location

--- application/__init__.py
+++ application/__init__.py
@@ -7,6 +7,7 @@ from werkzeug.serving import WSGIRequestHandler
 # enable persistent HTTP connections (keep-alive)
 WSGIRequestHandler.protocol_version = "HTTP/1.1"
 
+# launch the client from different path depending on whether the Python script has been packed into an executable
 if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
     app = Flask(__name__, static_folder=os.path.join(os.getcwd(), 'client'))
 else:
@@ -18,16 +19,16 @@ try:
     APP_PORT = sys.argv[1]
     APP_HOST = '127.0.0.1'
 
-    from .LocalProfile import LocalProfile
+    from .Profile.LocalProfile import LocalProfile
 
     profiles = LocalProfile()
 except IndexError:
-    # TODO: change this to 80 in production mode
+    # TODO: might change this to 80 in production mode
+    #   but it doesn't seem to be necessary at the moment
     APP_PORT = 5000
     APP_HOST = '0.0.0.0'
 
-    # fixme: change this
-    from .DBProfile import DBProfile
+    from .Profile.DBProfile import DBProfile
 
     profiles = DBProfile(app)
 
