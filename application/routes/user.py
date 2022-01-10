bb6b9b1ac30a4577e50f623479cbbfc0072a4c8f
Jiaxing Leo Li <leojiaxing.li@mail.utoronto.ca>
2022-01-10 13:58:48 -0500

    Add email activation

--- application/routes/user.py
+++ application/routes/user.py
@@ -53,3 +53,30 @@ def logout():
     profiles.logout()
 
     return f'logged out'
+
+
+@api.route('/resend', methods=['GET'])
+def resend():
+    try:
+        email = request.args['email']
+        profiles.send_activation_email(email)
+    except KeyError as e:
+        abort(403, f'{e} is missing')
+    return 'sent'
+
+
+@api.route('/activate', methods=['POST', 'GET'])
+def activate():
+    try:
+        if request.method == 'GET':
+            userid = request.args['userid']
+            code = request.args['code']
+        else:
+            userid = request.json['userid']
+            code = request.json['code']
+        if profiles.activate_user(userid, code):
+            return 'activated'
+    except KeyError as e:
+        abort(403, f'{e} is missing')
+
+    return 'failed to activate'
