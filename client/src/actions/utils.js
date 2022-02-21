2f388ae223fcd7bee236e693c3c504ea2bf1c2d2
Junhao Liao <junhao@junhao.ca>
2022-02-21 05:10:28 -0500

    [Login] added frontend and backend for user activation+login+register

--- client/src/actions/utils.js
+++ client/src/actions/utils.js
@@ -1,36 +1,39 @@
-export const htmlResponseToReason = (response) => {
-    const html = document.createElement('html');
-    html.innerHTML = response;
-    const reasonHTML = html.children[1];
-    try {
-        const reasonTitle = reasonHTML.children[0].innerText;
-        const reasonContent = reasonHTML.children[1].innerText;
-        return `${reasonTitle} - ${reasonContent}`;
-    } catch (_) {
-        return reasonHTML.innerText;
+export const htmlResponseToReason = (response, withoutStatus) => {
+  const html = document.createElement('html');
+  html.innerHTML = response;
+  const reasonHTML = html.children[1];
+  try {
+    const reasonContent = reasonHTML.children[1].innerText;
+    if (withoutStatus) {
+      return reasonContent;
     }
+    const reasonTitle = reasonHTML.children[0].innerText;
+    return `${reasonTitle} - ${reasonContent}`;
+  } catch (_) {
+    return reasonHTML.innerText;
+  }
 
 };
 
 // Reference: https://stackoverflow.com/questions/9038625/detect-if-device-is-ios
 export const isIOS = () => {
-    return [
-            'iPad Simulator',
-            'iPhone Simulator',
-            'iPod Simulator',
-            'iPad',
-            'iPhone',
-            'iPod',
-        ].includes(navigator.platform)
-        // iPad on iOS 13 detection
-        || (navigator.userAgent.includes('Mac') && 'ontouchend' in document);
+  return [
+        'iPad Simulator',
+        'iPhone Simulator',
+        'iPod Simulator',
+        'iPad',
+        'iPhone',
+        'iPod',
+      ].includes(navigator.platform)
+      // iPad on iOS 13 detection
+      || (navigator.userAgent.includes('Mac') && 'ontouchend' in document);
 };
 
 export const canChangeMachine = (hostname) => {
-    return hostname.endsWith('.eecg.toronto.edu') ||
-        hostname.endsWith('.ecf.toronto.edu') ||
-        hostname.endsWith('.eecg.utoronto.ca') ||
-        hostname.endsWith('.ecf.utoronto.ca');
+  return hostname.endsWith('.eecg.toronto.edu') ||
+      hostname.endsWith('.ecf.toronto.edu') ||
+      hostname.endsWith('.eecg.utoronto.ca') ||
+      hostname.endsWith('.ecf.utoronto.ca');
 };
 
 // ref: http://detectmobilebrowsers.com/
