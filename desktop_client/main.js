152f60d57eb9e5063f2c9891222c96afa8615eab
Junhao Liao <junhao@junhao.ca>
2022-02-22 01:17:49 -0500

    [Packaging] refactor code in main.js

--- desktop_client/main.js
+++ desktop_client/main.js
@@ -3,14 +3,13 @@ const {handleSquirrelEvent} = require('./squirrel_event');
 
 // this should be placed at top of main.js to handle setup events quickly
 if (handleSquirrelEvent()) {
-    // squirrel event handled and app will exit in 1000ms, so don't do anything else
-    return;
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
 }
 
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, nativeImage, Menu} = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
 const {spawn} = require('child_process');
-const {get} = require('http');
 const {resolve} = require('path');
 
 const {getFreePort} = require('./utils');
@@ -25,153 +24,170 @@ const isWindows = process.platform === 'win32';
 // the backend process handle
 let ictrl_be = null;
 if (isMac) {
-    ictrl_be = spawn('./ictrl_be', [mainPort], {cwd: resolve(__dirname, 'ictrl_be')});
+  ictrl_be = spawn('./ictrl_be', [mainPort],
+      {cwd: resolve(__dirname, 'ictrl_be')});
 
-    // need to have a 'window' role in the menu on mac
-    //  to show all windows when right-clicking on the dock icon
-    const menuTemplate = [{
-        role: 'window',
-        submenu: [
-            {
-                role: 'minimize'
-            },
-            {
-                role: 'close'
-            }
-        ]
+  // need to have a 'window' role in the menu on mac
+  //  to show all windows when right-clicking on the dock icon
+  const menuTemplate = [
+    {
+      role: 'window',
+      submenu: [{role: 'minimize'}, {role: 'close'}],
     }];
-    Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
 } else if (isWindows) {
-    ictrl_be = spawn('ictrl_be.exe', [mainPort], {cwd: resolve(__dirname, 'ictrl_be')});
-    Menu.setApplicationMenu(null);
+  ictrl_be = spawn('ictrl_be.exe', [mainPort],
+      {cwd: resolve(__dirname, 'ictrl_be')});
+  Menu.setApplicationMenu(null);
 } else if (isLinux) {
-    ictrl_be = spawn('./ictrl_be', [mainPort], {cwd: resolve(__dirname, 'ictrl_be')});
-    Menu.setApplicationMenu(null);
+  ictrl_be = spawn('./ictrl_be', [mainPort],
+      {cwd: resolve(__dirname, 'ictrl_be')});
+  Menu.setApplicationMenu(null);
 } else {
-    console.log(`OS: ${process.platform} not supported.`)
-    app.exit()
+  console.log(`OS: ${process.platform} not supported.`);
+  app.exit();
 }
 
+const setupNewWindowRecognitionMac = (openedWindows, newWindow, mainWindow) => {
+  const newWindowKey = new Date().getTime();
+  openedWindows[newWindowKey] = newWindow;
+  newWindow.on('close', () => {
+    delete openedWindows[newWindowKey];
+    if (mainWindow) {
+      mainWindow.show();
+    }
+  });
+
+  newWindow.on('show', () => {
+    for (let key in openedWindows) {
+      if (openedWindows[key] !== newWindow) {
+        openedWindows[key].minimize();
+      }
+    }
+  });
+};
+
+function setupNewWindowIcon(url, newWindow) {
+  const {nativeImage} = require('electron');
+  const {get} = require('http');
+
+  const split = url.split('/');
+  const sessionId = split[split.length - 1];
+  const feature = split[split.length - 2];
+  get({
+    hostname: '127.0.0.1',
+    port: mainPort,
+    path: `/api/favicon/${feature}/${sessionId}`,
+  }, (msg) => {
+    const result = [];
+
+    msg.on('data', (data) => {
+      result.push(data);
+    });
+
+    msg.on('end', () => {
+      const icon = nativeImage.createFromBuffer(Buffer.concat(result));
+      newWindow.setIcon(icon);
+    });
+  });
+}
+
+const setupNewWindowRecognitionWindows = newWindow => {
+  const url = newWindow.getURL();
+  newWindow.setAppDetails({
+    appId: url,
+  });
+  setupNewWindowIcon(url, newWindow);
+};
+
+const setupNewWindowReadyToShow = (newWindow, openedWindows, mainWindow) => {
+  newWindow.once('ready-to-show', () => {
+    // improve recognition of each window in the taskbar
+    if (isMac) {
+      setupNewWindowRecognitionMac(openedWindows, newWindow, mainWindow);
+    } else if (isWindows) {
+      setupNewWindowRecognitionWindows(newWindow);
+    }
+
+    newWindow.show();
+    newWindow.maximize();
+  });
+};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
+const windowOptions = {
+  width: 800,
+  height: 600,
+  show: false,
+  webPreferences: {
+    nativeWindowOpen: true,
+  },
+};
 app.whenReady().then(() => {
-    // Create the browser window.
-    let mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        show: false,
-        webPreferences: {
-            nativeWindowOpen: true
+  // Create the browser window.
+  let mainWindow = new BrowserWindow(windowOptions);
+
+  // load backend dashboard
+  mainWindow.setTitle('Loading... ');
+  mainWindow.loadURL(`http://127.0.0.1:${mainPort}/dashboard`).then();
+  // need to reload on Mac because the first load times out very quickly
+  mainWindow.webContents.on('did-fail-load', () => {
+    mainWindow.reload();
+  });
+  mainWindow.webContents.on('did-finish-load', () => {
+    mainWindow.show();
+    mainWindow.maximize();
+  });
+
+  const openedWindows = {};
+  if (isMac) {
+    // list to keep track of all opened windows for minimization into the Dock's "Recent Applications"
+    openedWindows['main'] = mainWindow;
+    mainWindow.on('close', () => {
+      mainWindow = null;
+      delete openedWindows['main'];
+    });
+    mainWindow.on('show', () => {
+      for (let key in openedWindows) {
+        if (key !== 'main') {
+          openedWindows[key].minimize();
         }
+      }
     });
-
-    mainWindow.setTitle('Loading... ');
-
-    mainWindow.loadURL(`http://127.0.0.1:${mainPort}/dashboard`);
-    // need to reload on Mac because the first load times out very quickly
-    mainWindow.webContents.on('did-fail-load', () => {
-        mainWindow.reload();
+  } else if (isWindows) {
+    mainWindow.setAppDetails({
+      appId: 'iCtrl',
     });
+  }
 
-    mainWindow.webContents.on('did-finish-load', () => {
-        mainWindow.show();
-        mainWindow.maximize();
-    });
+  mainWindow.webContents.on('new-window', (event, url) => {
+    event.preventDefault();
 
-    const openedWindows = {};
-    if (isMac) {
-        // list to keep track of all opened windows for minimization into the Dock's "Recent Applications"
-        openedWindows['main'] = mainWindow;
-        mainWindow.on('close', () => {
-            mainWindow = null;
-            delete openedWindows['main'];
-        });
-        mainWindow.on('show', () => {
-            for (let key in openedWindows) {
-                if (key !== 'main') {
-                    openedWindows[key].minimize();
-                }
-            }
-        });
-    } else if (isWindows) {
-        mainWindow.setAppDetails({
-            appId: 'iCtrl'
-        });
-    }
+    const newWindow = new BrowserWindow(windowOptions);
+    newWindow.loadURL(url).then();
+    setupNewWindowReadyToShow(newWindow, openedWindows, mainWindow);
 
-    mainWindow.webContents.on('new-window', (event, url) => {
-        event.preventDefault();
+  });
 
-        const newWindow = new BrowserWindow({
-            width: 800,
-            height: 600,
-            show: false,
-            webPreferences: {
-                nativeWindowOpen: true
-            }
-        });
-        newWindow.loadURL(url);
-
-        newWindow.once('ready-to-show', () => {
-            // improve recognition of each window in the taskbar
-            if (isMac) {
-                const newWindowKey = new Date().getTime();
-                openedWindows[newWindowKey] = newWindow;
-                newWindow.on('close', () => {
-                    delete openedWindows[newWindowKey];
-                    if (mainWindow) {
-                        mainWindow.show();
-                    }
-                });
-
-                newWindow.on('show', () => {
-                    for (let key in openedWindows) {
-                        if (openedWindows[key] !== newWindow) {
-                            openedWindows[key].minimize();
-                        }
-                    }
-                });
-            } else if (isWindows) {
-                const url = newWindow.getURL();
-                newWindow.setAppDetails({
-                    appId: url
-                });
-                const split = url.split('/');
-                const sessionId = split[split.length - 1];
-                const feature = split[split.length - 2];
-                get({
-                    hostname: '127.0.0.1',
-                    port: mainPort,
-                    path: `/api/favicon/${feature}/${sessionId}`,
-                }, (res) => {
-                    res.on('data', (data) => {
-                        const icon = nativeImage.createFromBuffer(data);
-                        newWindow.setIcon(icon);
-                    });
-                });
-            }
-
-            newWindow.show();
-            newWindow.maximize();
-        });
-
-    });
-
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 });
 
 // Quit when all windows are closed
 app.on('window-all-closed', () => {
-    app.quit();
+  app.quit();
 });
 
+// kill the backend when the app exits
 app.on('before-quit', () => {
-    // kill the backend when the app exits
-    ictrl_be.kill('SIGKILL');
+  ictrl_be.kill('SIGTERM');
+
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// in case the above is not successful,
+//  force kill the backend when the process exits
+process.on('exit', () => {
+  ictrl_be.kill('SIGKILL');
+});
