013543bd8d2aed77e2d4971de67f816da2dd1724
Junhao Liao <junhao@junhao.ca>
2022-02-24 03:55:58 -0500

    [Home] use GitHub official component to display GitHub stars

--- client/src/interface/utils.js
+++ client/src/interface/utils.js
@@ -2,11 +2,3 @@ export const changeFavicon = (href) => {
   const link = document.getElementById('favicon');
   link.href = href;
 };
-
-export const addScript = (scriptLink) => {
-  const script = document.createElement('script');
-  script.src = scriptLink;
-  script.async = true;
-
-  document.body.appendChild(script);
-};
