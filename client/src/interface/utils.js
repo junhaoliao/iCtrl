cd04994806a03583d2b35b458f234c5a31875f43
Junhao Liao <junhao@junhao.ca>
2022-02-24 03:48:06 -0500

    [Home] improve home presentation

--- client/src/interface/utils.js
+++ client/src/interface/utils.js
@@ -1,4 +1,12 @@
 export const changeFavicon = (href) => {
-    const link = document.getElementById('favicon');
-    link.href = href;
+  const link = document.getElementById('favicon');
+  link.href = href;
+};
+
+export const addScript = (scriptLink) => {
+  const script = document.createElement('script');
+  script.src = scriptLink;
+  script.async = true;
+
+  document.body.appendChild(script);
 };
