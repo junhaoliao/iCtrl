73c69337a678b46884a665b47091f13de61a0968
Junhao Liao <junhao@junhao.ca>
2022-02-26 09:45:19 -0500

    [Mobile] add minWidth in the columns of CheckLoad

--- client/src/interface/components/ChangeMachine/column.js
+++ client/src/interface/components/ChangeMachine/column.js
@@ -1,29 +1,34 @@
 const columns = [
-    {
-        field: 'id',
-        headerName: 'Host',
-        flex: 1,
-    },
-    {
-        field: 'userNum',
-        headerName: 'User Count',
-        flex: 1,
-    },
-    {
-        field: 'load1',
-        headerName: '1-min Avg',
-        flex: 1,
-    },
-    {
-        field: 'load5',
-        headerName: '5-min Avg',
-        flex: 1,
-    },
-    {
-        field: 'load15',
-        headerName: '15-min Avg',
-        flex: 1,
-    },
+  {
+    field: 'id',
+    headerName: 'Host',
+    minWidth: 115,
+    flex: 1,
+  },
+  {
+    field: 'userNum',
+    headerName: 'User Count',
+    minWidth: 160,
+    flex: 1,
+  },
+  {
+    field: 'load1',
+    headerName: '1-min Avg',
+    minWidth: 150,
+    flex: 1,
+  },
+  {
+    field: 'load5',
+    headerName: '5-min Avg',
+    minWidth: 150,
+    flex: 1,
+  },
+  {
+    field: 'load15',
+    headerName: '15-min Avg',
+    minWidth: 160,
+    flex: 1,
+  },
 ];
 
 export default columns;
