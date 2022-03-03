ced0010f2ee80c8de9182689ce42dcd7da8dae76
Junhao Liao <junhao@junhao.ca>
2022-03-03 10:28:56 -0500

    [ChangeMachine] reduce column min size

--- client/src/interface/components/ChangeMachine/column.js
+++ client/src/interface/components/ChangeMachine/column.js
@@ -2,31 +2,31 @@ const columns = [
   {
     field: 'id',
     headerName: 'Host',
-    minWidth: 120,
+    minWidth: 100,
     flex: 1,
   },
   {
     field: 'userNum',
     headerName: 'User Count',
-    minWidth: 180,
+    minWidth: 140,
     flex: 1,
   },
   {
     field: 'load1',
     headerName: '1-min Avg',
-    minWidth: 160,
+    minWidth: 140,
     flex: 1,
   },
   {
     field: 'load5',
     headerName: '5-min Avg',
-    minWidth: 160,
+    minWidth: 140,
     flex: 1,
   },
   {
     field: 'load15',
     headerName: '15-min Avg',
-    minWidth: 160,
+    minWidth: 140,
     flex: 1,
   },
 ];
