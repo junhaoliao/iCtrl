49c74e17722538fb992dfacc428a339bfb147dac
IreneLi <irene2002yr@gmail.com>
2024-09-26 23:04:54 -0400

    Integrate logging to application/features/.

--- application/features/vncpasswd.py
+++ application/features/vncpasswd.py
@@ -19,6 +19,9 @@
 #   IN THE SOFTWARE.
 
 import pyDes
+import logging.config
+
+logger = logging.getLogger(__name__)
 
 KEY = [232, 74, 214, 96, 196, 114, 26, 224]
 CRYPT = pyDes.des(KEY)
@@ -42,6 +45,7 @@ def vncpasswd(passwd_path, password=None):
 
     with open(passwd_path, "wb") as passwd:
         passwd.write(obfuscated)
+        logger.debug("Vnc Password: Write obfuscated password to {}".format(passwd_path))
 
     return obfuscated
 
