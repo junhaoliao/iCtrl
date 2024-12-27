49d84fc4c7d3f2753da2c92880092eb3d24e0bed
Ruihao Li <a834878215@gmail.com>
2024-12-27 15:37:27 -0500

    Minor change from code review

--- application/utils.py
+++ application/utils.py
@@ -101,7 +101,7 @@ def get_headers_dict_from_str(headers_str):
         header_name, header_value = line.split(': ', 1)
         headers[header_name] = header_value
 
-    logger.debug(f'Extracted HTTP headers, headers = %s', headers)
+    logger.debug('Extracted HTTP headers, headers = %s', headers)
 
     return headers
 
